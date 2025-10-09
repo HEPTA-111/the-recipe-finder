d5173db017e26878b5311a919750ca33eb32d27f
Hepta-111 <oderahoward@gmail.com>
2025-10-09 17:22:22 +0300

    UI Update

--- the-recipe-finder/src/components/Header.tsx
+++ the-recipe-finder/src/components/Header.tsx
@@ -1,21 +1,17 @@
-export default  function Header() {
+export default function Header() {
     return (
         <header className="header">
-            <h1 className="logo">The Recipe Finder</h1>
-            <div  className="nav-links"> 
-                <p>Favourites ❤️</p>
+            <div className="header-content">
+                <h1>The Recipe Finder</h1>
+                <div className="nav-links">
+                    <p>Favourites ❤️</p>
+                    <p>About</p>
+                </div>
+                <div className="search-bar">
+                    <input type="text" className="search-input" placeholder="Search for recipes..." />
+                    <button className="search-button">Search</button>
+                </div>
             </div>
-
-            <div className="search-bar">
-                <input type="text" className="search-input" placeholder="Search for recipes..." />
-                <button className="search-button">Search</button>
-            </div>
-            
-            <div className="nav-links">
-                <p>About</p>
-
-            </div>
-
         </header>
     );
-}   
+}
